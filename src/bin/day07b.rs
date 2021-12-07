f19f4c53a4ddcd8d640cdecc6ad2a11ece3ee1f3
Orson Peters <orsonpeters@gmail.com>
2021-12-08 00:21:03 +0100

    Made day 7 proof more rigorous.

--- src/bin/day07b.rs
+++ src/bin/day07b.rs
@@ -3,14 +3,22 @@ use itertools::Itertools;
 use std::cmp;
 
 /*
-    Let f(a) = total fuel cost to align to position a.
+    Let fuel(a) = total fuel cost to align to position a.
     Note: 1 + 2 + 3 + ... + k = k*(k+1)/2.
-    Minimize: f(a) = sum(abs(x_i - a)*(1 + abs(x_i - a))/2) for all x_i in positions.
-    Decompose: f(a) = sum((x_i - a)^2) + sum(abs(x_i - a)) / 2
-    Opt f'(a) = 0 : sum(x_i - a) + sum(-sgn(x_i - a)) / 2 = 0
-    Rearrange:      sum(x_i) - sum(sgn(x_i - a)) / 2 = n*a
-    Rearrange:      mean(x_i) - sum(sgn(x_i - a)) / (2*n) = a
-    Bound:          mean(x_i) - 1/2 <= a <= mean(x_i) + 1/2
+    fuel(a) = sum(abs(x_i - a)(1 + abs(x_i - a))) / 2
+    fuel_left(a) = sum((a - x_i)(1 + a - x_i) for x_i < a) / 2    partition
+    fuel_right(a) = sum((x_i - a)(1 + x_i - a) for x_i >= a) / 2
+    fuel_left'(a) = sum(2(a - x_i) + 1 for x_i < a) / 2           differentiate
+    fuel_right'(a) = sum(2(a - x_i) - 1 for x_i >= a) / 2
+    fuel'(a) = sum(a - x_i) + (count(x_i < a) - count(x_i >= a)) / 2
+    count(x_i >= a) = n - count(x_i < a)                          observation
+    fuel'(a) = sum(a - x_i) + (2*count(x_i < a) - n) / 2
+    fuel'(a) = n*a - sum(x_i) + count(x_i < a) - n/2
+
+    n*a - sum(x_i) + count(x_i < a) - n/2 = 0                     fuel'(a) = 0
+    a = mean(x_i) + 1/2 - count(x_i < a)/n
+
+    Finally since count(x_i < a)/n lies in [0, 1] we have a in mean(x_i) +/- 1/2.
 */
 
 fn cost(positions: &[i64], a: i64) -> i64 {
