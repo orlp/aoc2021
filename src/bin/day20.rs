eb9ea309e44e09320f756b1023976a0c3a76a102
Orson Peters <orsonpeters@gmail.com>
2021-12-24 16:52:24 +0100

    Small stylistic change day 20.

--- src/bin/day20.rs
+++ src/bin/day20.rs
@@ -10,30 +10,30 @@ fn parse_pixels(line: &str) -> impl Iterator<Item = Result<bool>> + '_ {
     })
 }
 
-fn simulate(algo: &[bool], img: Vec<bool>, width: usize, infinity: bool) -> (Vec<bool>, usize, bool) {
+fn simulate(algo: &[bool], img: Vec<bool>, width: usize, infty: bool) -> (Vec<bool>, usize, bool) {
     let (w, h) = (width as i64, (img.len() / width) as i64);
     let mut new_img = vec![false; ((w + 2) * (h + 2)) as usize];
 
-    let mut prev_row = vec![511 * infinity as usize; width + 2];
+    let mut prev_row = vec![511 * infty as usize; width + 2];
     for y in 0..h + 2 {
         for x in 0..w + 2 {
             let mut idx = prev_row[x as usize];
             for dx in [-2, -1, 0] {
                 idx <<= 1;
                 if 0 <= x + dx && x + dx < w && 0 <= y && y < h {
-                    idx |= img[(y*w + x + dx) as usize] as usize;
+                    idx |= img[(y * w + x + dx) as usize] as usize;
                 } else {
-                    idx |= infinity as usize;
+                    idx |= infty as usize;
                 }
             }
             idx &= 511;
 
-            new_img[(y*(w + 2) + x) as usize] = algo[idx];
+            new_img[(y * (w + 2) + x) as usize] = algo[idx];
             prev_row[x as usize] = idx;
         }
     }
 
-    (new_img, width + 2, algo[511 * infinity as usize])
+    (new_img, width + 2, algo[511 * infty as usize])
 }
 
 fn main() -> Result<()> {
@@ -41,11 +41,10 @@ fn main() -> Result<()> {
     let start = std::time::Instant::now();
     let (algo, image) = input.split_once('\n').context("no newline")?;
     let algo: Vec<bool> = parse_pixels(algo).try_collect()?;
-    let image: Vec<Vec<bool>> = image.trim().lines().map(|l| Ok(parse_pixels(l).try_collect()?)).try_collect()?;
-    let width = image[0].len();
-    let flat_image = image.into_iter().flatten().collect_vec();
+    let width = image.trim().split_once('\n').context("no image")?.0.len();
+    let image: Vec<bool> = image.lines().flat_map(|l| parse_pixels(l)).try_collect()?;
 
-    let mut state = (flat_image, width, false);
+    let mut state = (image, width, false);
     state = simulate(&algo, state.0, state.1, state.2);
     state = simulate(&algo, state.0, state.1, state.2);
     let part1 = state.0.iter().map(|x| *x as u64).sum::<u64>();
