20aa708e1402b7632f770ce3dbbdbed0be0fba32
Orson Peters <orsonpeters@gmail.com>
2021-12-25 00:07:38 +0100

    Solved day 24 without assuming anything about the input.

---
+++ src/bin/day24.rs
@@ -0,0 +1,126 @@
+use anyhow::{Context, Result};
+use derive_more::TryInto;
+use hashbrown::HashMap;
+use itertools::Itertools;
+
+type Word = i32;
+type Register = u8;
+type RegFile = [Word; 4];
+
+#[derive(Debug, Clone, Copy, TryInto)]
+enum Operand {
+    Immediate(Word),
+    Register(Register),
+}
+
+#[derive(Debug, Clone, Copy)]
+enum Instr {
+    Inp(Register),
+    Add(Register, Operand),
+    Mul(Register, Operand),
+    Div(Register, Operand),
+    Mod(Register, Operand),
+    Eql(Register, Operand),
+}
+
+impl Operand {
+    fn value(self, regs: &RegFile) -> Word {
+        match self {
+            Operand::Immediate(x) => x,
+            Operand::Register(r) => regs[r as usize],
+        }
+    }
+}
+
+const INPUTS: [[i32; 9]; 2] = [[9, 8, 7, 6, 5, 4, 3, 2, 1], [1, 2, 3, 4, 5, 6, 7, 8, 9]];
+
+type Cache = HashMap<RegFile, Option<u64>>;
+fn find_sat_input(
+    code: &[Instr],
+    mut regs: RegFile,
+    mut ip: u32,
+    caches: &mut [Cache],
+    min: bool,
+) -> Option<u64> {
+    let (init_regs, init_ip) = (regs, ip);
+    if let Some(ret) = caches[init_ip as usize].get(&init_regs) {
+        return *ret;
+    }
+
+    while let Some(instr) = code.get(ip as usize) {
+        match instr {
+            Instr::Inp(dst) => {
+                let mut res = None;
+                for input in INPUTS[min as usize] {
+                    regs[*dst as usize] = input;
+                    if let Some(sat) = find_sat_input(code, regs, ip + 1, caches, min) {
+                        res = Some(10 * sat + input as u64);
+                        break;
+                    }
+                }
+                caches[init_ip as usize].insert(init_regs, res);
+                return res;
+            },
+            Instr::Add(dst, src) => regs[*dst as usize] += src.value(&regs),
+            Instr::Mul(dst, src) => regs[*dst as usize] *= src.value(&regs),
+            Instr::Div(dst, src) => regs[*dst as usize] /= src.value(&regs),
+            Instr::Mod(dst, src) => regs[*dst as usize] %= src.value(&regs),
+            Instr::Eql(dst, src) => {
+                regs[*dst as usize] = (regs[*dst as usize] == src.value(&regs)) as Word
+            },
+        }
+        ip += 1;
+    }
+
+    let res = (regs[3] == 0).then(|| 0);
+    caches[init_ip as usize].insert(init_regs, res);
+    res
+}
+
+fn parse_register(s: &str) -> Result<Register> {
+    let is_reg = matches!(s, "w" | "x" | "y" | "z");
+    is_reg.then(|| s.as_bytes()[0] - b'w').context("unknown register")
+}
+
+fn parse_operand(s: &str) -> Result<Operand> {
+    let reg = parse_register(s).map(Operand::Register);
+    reg.or_else(|_| Ok(Operand::Immediate(s.parse()?)))
+}
+
+fn main() -> Result<()> {
+    let input = std::fs::read_to_string("inputs/day24.txt")?;
+    let start = std::time::Instant::now();
+    let mut instructions = Vec::new();
+    for line in input.lines() {
+        let components = line.split_ascii_whitespace().collect_vec();
+        let instr = match &components[..] {
+            &["inp", r] => Instr::Inp(parse_register(r)?),
+            &["add", r, o] => Instr::Add(parse_register(r)?, parse_operand(o)?),
+            &["mul", r, o] => Instr::Mul(parse_register(r)?, parse_operand(o)?),
+            &["div", r, o] => Instr::Div(parse_register(r)?, parse_operand(o)?),
+            &["mod", r, o] => Instr::Mod(parse_register(r)?, parse_operand(o)?),
+            &["eql", r, o] => Instr::Eql(parse_register(r)?, parse_operand(o)?),
+            _ => anyhow::bail!("unknown instruction"),
+        };
+        instructions.push(instr);
+    }
+
+    let p1start = std::time::Instant::now();
+    let mut cache = vec![Cache::new(); instructions.len()];
+    let part1 = find_sat_input(&instructions, [0; 4], 0, &mut cache, false);
+    println!("part1 time: {:?}", p1start.elapsed());
+    println!(
+        "part1: {}",
+        part1.context("no solution")?.to_string().chars().rev().join("")
+    );
+    let p2start = std::time::Instant::now();
+    let mut cache = vec![Cache::new(); instructions.len()];
+    let part2 = find_sat_input(&instructions, [0; 4], 0, &mut cache, true);
+    println!("part2 time: {:?}", p2start.elapsed());
+    println!(
+        "part2: {}",
+        part2.context("no solution")?.to_string().chars().rev().join("")
+    );
+    println!("time: {:?}", start.elapsed());
+    Ok(())
+}
