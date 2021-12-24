5f13d5dc00b958e660530dfa9ab7ccc4bad88716
Orson Peters <orsonpeters@gmail.com>
2021-12-24 02:24:12 +0100

    Improved day 23 with A*, a decent heuristic and deadlock detection.

--- src/bin/day23.rs
+++ src/bin/day23.rs
@@ -1,9 +1,11 @@
 use std::cmp::Reverse;
 use std::collections::BinaryHeap;
-use hashbrown::HashMap;
 
 use anyhow::{Context, Result};
+use hashbrown::HashMap;
 use itertools::Itertools;
+
+const POW10: [u64; 4] = [1, 10, 100, 1000];
 
 /*
     State: for logic simplicity we use 0, 1, 3, 5, 7, 9, 10 as the valid hallway positions.
@@ -30,16 +32,30 @@ fn gen_moves<const N: usize>(state: &[u8; N], moves: &mut Vec<(usize, usize, usi
         let unobstructed = || path.clone().all(|i| i == hallway || state[i] == 0);
 
         if to_room {
+            // From hallway to room.
             let fill_ready = room_states[room].iter().all(|s| *s == 0 || *s as usize == room + 1);
             if fill_ready && unobstructed() {
                 let vacant_idx = room_first_occupied[room].unwrap_or(room_size) - 1;
                 moves.push((hallway, room_start(room) + vacant_idx, path_len + vacant_idx));
             }
         } else {
+            // From room to hallway.
             if let Some(occupied_idx) = room_first_occupied[room] {
                 let target_room = room_states[room][occupied_idx] as usize - 1;
-                let between = symm_range(2 + 2 * room, 2 + 2 * target_room).contains(&hallway);
-                if !(between && path_len > 2) && unobstructed() {
+
+                // Will this move cut off access irrecoverably?
+                let deadlock = room_states[target_room].iter().any(|s| {
+                    *s > 0 && {
+                        let required_path =
+                            symm_range(2 + 2 * target_room, 2 + 2 * (*s - 1) as usize);
+                        required_path.contains(&hallway)
+                    }
+                });
+
+                // To reduce superfluous nodes if we're moving directly to our target
+                // room, only ever take 1 step.
+                let direct_route = symm_range(2 + 2 * room, 2 + 2 * target_room).contains(&hallway);
+                if !(direct_route && path_len > 2) && !deadlock && unobstructed() {
                     moves.push((room_start(room) + occupied_idx, hallway, path_len + occupied_idx));
                 }
             }
@@ -57,17 +73,53 @@ fn gen_moves<const N: usize>(state: &[u8; N], moves: &mut Vec<(usize, usize, usi
     }
 }
 
-fn dijkstra_fuel_cost<const N: usize>(state: [u8; N]) -> Option<u64> {
-    let mut to_visit = BinaryHeap::from([(Reverse(0), state)]);
-    let mut min_cost = HashMap::new();
-    let mut moves = Vec::new();
-    min_cost.insert(state, 0);
+fn heuristic_fuel_cost<const N: usize>(state: &[u8; N]) -> u64 {
+    // Compute cost as if all amphipods can phase through eachother.
+    let room_size = (N - 11) / 4;
+    let mut hcost = 0;
+    let mut num_not_in_room = [0; 4];
 
-    while let Some((cost, state)) = to_visit.pop() {
-        if cost.0 > *min_cost.get(&state).unwrap_or(&u64::MAX) {
-            continue; // This means we already visited this node earlier.
+    // Cost to move inside the hallway to in front of the target room.
+    for i in 0..11 {
+        if let Some(target_room) = state[i].checked_sub(1) {
+            num_not_in_room[target_room as usize] += 1;
+            let dist = (i as i64 - (2 + 2 * target_room) as i64).abs() as u64;
+            hcost += dist * POW10[state[i] as usize - 1];
+        }
+    }
+
+    // Cost to move from inside current room to hallway in front of target room.
+    for room in 0..4 {
+        for offset in 0..room_size {
+            let i = 11 + room_size * room + offset;
+            if let Some(target_room) = state[i].checked_sub(1) {
+                if target_room as usize != room {
+                    num_not_in_room[target_room as usize] += 1;
+                    let hallway_path_len = 2 * (room as i64 - target_room as i64).abs() as u64;
+                    let exit_dist = 1 + offset as u64;
+                    hcost += (exit_dist + hallway_path_len) * POW10[state[i] as usize - 1];
+                }
+            }
+        }
+    }
+
+    // Total cost for k amphipods in front of room to enter.
+    for (i, k) in num_not_in_room.into_iter().enumerate() {
+        hcost += k * (k + 1) / 2 * POW10[i];
+    }
+
+    hcost
+}
+
+fn astar_fuel_cost<const N: usize>(state: [u8; N]) -> Option<u64> {
+    let mut to_visit = BinaryHeap::from([(Reverse(0), 0, state)]);
+    let mut min_cost: HashMap<[u8; N], u64> = [(state, 0)].into_iter().collect();
+    let mut moves = Vec::new();
+    while let Some((_hcost, cost, state)) = to_visit.pop() {
+        if cost > *min_cost.get(&state).unwrap_or(&u64::MAX) {
+            continue; // We got a better estimate in the meantime.
         } else if state.windows(2).all(|w| w[0] <= w[1]) {
-            return Some(cost.0); // First time we visit a node is optimal - return.
+            return Some(cost); // First time we visit a node is optimal - return.
         }
 
         gen_moves(&state, &mut moves);
@@ -76,10 +128,11 @@ fn dijkstra_fuel_cost<const N: usize>(state: [u8; N]) -> Option<u64> {
             let amphi = new_state[from];
             new_state[to] = amphi;
             new_state[from] = 0;
-            let new_cost = cost.0 + dist as u64 * 10u64.pow(amphi as u32 - 1);
+            let new_cost = cost + dist as u64 * POW10[amphi as usize - 1];
             if new_cost < *min_cost.get(&new_state).unwrap_or(&u64::MAX) {
                 min_cost.insert(new_state, new_cost);
-                to_visit.push((Reverse(new_cost), new_state));
+                let heuristic_cost = new_cost + heuristic_fuel_cost(&new_state);
+                to_visit.push((Reverse(heuristic_cost), new_cost, new_state));
             }
         }
     }
@@ -96,13 +149,14 @@ fn parse_state<const N: usize>(s: &str) -> [u8; N] {
     state
 }
 
+
 fn main() -> Result<()> {
     let input = std::fs::read_to_string("inputs/day23.txt")?;
     let start = std::time::Instant::now();
     let mut part2_input = input.lines().collect_vec();
     part2_input.splice(3..3, ["#D#C#B#A#", "#D#B#A#C#"]);
-    let part1 = dijkstra_fuel_cost(parse_state::<{ 11 + 2 * 4 }>(&input));
-    let part2 = dijkstra_fuel_cost(parse_state::<{ 11 + 4 * 4 }>(&part2_input.join("")));
+    let part1 = astar_fuel_cost(parse_state::<{ 11 + 2 * 4 }>(&input));
+    let part2 = astar_fuel_cost(parse_state::<{ 11 + 4 * 4 }>(&part2_input.join("")));
 
     println!("time: {:?}", start.elapsed());
     println!("part1: {}", part1.context("no part 1 solution")?);
